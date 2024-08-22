9d1b0fe6eb2c525714778d4b30d3f29044a47aa7
Khoa Nguyen <n.d.khoa511@gmail.com>
2024-08-22 22:30:07 +0700

    integrate with notion

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,60 @@
-import express, { Request, Response } from "express";
+require("dotenv").config();
+
+import express, { NextFunction, Request, Response } from "express";
+import { Client } from "@notionhq/client";
+
+interface ThingToLearn {
+  label: string;
+  url: string;
+}
+
+// The dotenv library will read from your .env file into these values on `process.env`
+const notionDatabaseId = process.env.NOTION_DB_ID;
+const notionSecret = process.env.NOTION_SECRET;
+
+// Will provide an error to users who forget to create the .env file
+// with their Notion data in it
+if (!notionDatabaseId || !notionSecret) {
+  throw Error("Must define NOTION_SECRET and NOTION_DB_ID in env");
+}
+
+// Initializing the Notion client with your secret
+const notion = new Client({
+  auth: notionSecret,
+});
 
 const app = express();
 const port = 3000;
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 // GET request handler
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+app.get("/", async (req: Request, res: Response) => {
+  const query = await notion.databases.query({
+    database_id: notionDatabaseId,
+  });
+  const list = query.results.map((row) => {
+    if (!("properties" in row)) {
+      return { label: "NOT_FOUND", url: "" };
+    }
+
+    const labelCell = row.properties.label;
+    const urlCell = row.properties.url;
+
+    const isLabel = labelCell.type === "rich_text";
+    const isUrl = urlCell.type === "url";
+
+    if (isLabel && isUrl && labelCell.rich_text instanceof Array) {
+      const label = labelCell.rich_text[0].plain_text;
+      const url = urlCell.url ?? "";
+
+      return { label, url };
+    }
+  });
+  res.json(list);
 });
 
 app.listen(port, () => {
